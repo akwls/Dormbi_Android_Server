31be14159f9ab0e8272af173e98328df03dbd8c2
Hajin Kim <rhrnak624@naver.com>
2021-11-15 21:42:46 +0900

    속성 이름 변경

--- router/user.js
+++ router/user.js
@@ -36,8 +36,8 @@ router.post('/join', function (req, res) {
   const user = req.body;
   //const user = user_join_test;
   const salt = 10;
-  const password = bcryptjs.hashSync(user.pw, salt); // 비밀번호 암호화
-  var sqlIDCheck = 'select * from user where UserID = ?';
+  const password = bcryptjs.hashSync(user.UserPW, salt); // 비밀번호 암호화
+  var sqlIDCheck = 'select * from USER where UserID = ?';
   var sqlRoomCheck = 'select * from student where StuNO = ? && StuName = ?';
   var query = sqlIDCheck;
 
@@ -52,7 +52,7 @@ router.post('/join', function (req, res) {
         console.log(err);
       }
       else {
-        connection.query(sqlRoomCheck, [user.num, user.name], function(err, result1) {
+        connection.query(sqlRoomCheck, [user.UserNO, user.UserNAME], function(err, result1) {
           if(result1.length !== 1) {
             res.json({
               message: "기숙사생 정보와 일치하지 않습니다."
@@ -63,7 +63,7 @@ router.post('/join', function (req, res) {
               console.log(err);
             }
             else {
-              connection.query(`INSERT INTO USER VALUES ('${user.id}', '${password}', '${user.name}', '${user.num}', '${user.tel}', '${user.ptel}', '${user.loc}')`, function (err, result2) {
+              connection.query(`INSERT INTO USER VALUES ('${user.UserID}', '${password}', '${user.UserNAME}', '${user.UserNO}', '${user.UserTEL}', '${user.UserP_TEL}', '${user.UserLOC}')`, function (err, result2) {
                 let resultCode = 404;
                 let message = '에러가 발생했습니다';
                 if (err)
@@ -88,8 +88,8 @@ router.post('/join', function (req, res) {
 
 //로그인
 router.post('/login', function (req, res) {
-  const id = req.body.id;
-  const pw = req.body.pw;
+  const id = req.body.UserID;
+  const pw = req.body.UserPW;
   const sql = 'select * from USER where UserID = ?';
   connection.query(sql, id, function (err, result) {
     let resultCode = 404;
