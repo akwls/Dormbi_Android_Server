318652dd059f02bc4b20ecbae34642509ff5643d
Hajin Kim <rhrnak624@naver.com>
2021-11-25 22:53:49 +0900

    go out 라우터 코드 추가

--- router/goout.js
+++ router/goout.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express();
 const connection = require('../mysql');
 
+router.use(express.urlencoded({
+    extended: false
+  })); //application/x-www-form-urlencoded
+  router.use(express.json());
+  
 router.get('/', function(req, res) {
     res.send('goout');
 });
