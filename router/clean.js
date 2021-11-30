78e178a2c40b72d0017e96b25db45327bbddf189
Hajin Kim <rhrnak624@naver.com>
2021-11-30 22:40:55 +0900

    층별 청소당번 출력

--- router/clean.js
+++ router/clean.js
@@ -11,10 +11,10 @@ router.get('/', function (req, res) {
     res.send('clean');
 });
     
-router.get('/cleanlist/:start', function(req, res) {
+router.get('/cleanlist/:start/:floor', function(req, res) {
     let start = req.params.start;
-    const sql = "select RoomNO, RoomNum, RoomFloor, CASE when RoomNum >= ? then '1' else '0' end NumPriority from ROOM order by NumPriority desc, RoomNum asc limit 7";
-    connection.query(sql, start, function(err, result) {
+    const sql = "select RoomNO, RoomNum, RoomFloor, CASE when RoomNum >= ? then '1' else '0' end NumPriority from ROOM where RoomFloor = ? order by NumPriority desc, RoomNum asc limit 7";
+    connection.query(sql, [start, floor], function(err, result) {
         if(err) {
             return res.sendStatus(400); 
         }
